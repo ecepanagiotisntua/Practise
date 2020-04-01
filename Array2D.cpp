00cd53d215c5d3f9e3260ba70ac0d9307df32b50
ecepanagiotisntua <pkaoukis.2001@yahoo.gr>
2020-04-01 20:59:01 +0000

    2 more methods

--- Array2D.cpp
+++ Array2D.cpp
@@ -9,17 +9,36 @@ class Array2D{
     Array2D(int n = 0, int m = 0, int bi = 0, int bj = 0);
     Array2D(const Array2D &a);
     ~Array2D();
-    Array2D & operator = (const Array2D &a);
+    Array2D & operator = (const Array2D &a){
+      delete data[];
+      data = new T[a.length];
+      basei = a.basei;
+      basej = a.basej;
+      length = a.length;
+      for (int i = 0, i < length; i++) data[i] = a.data[i];
+    }
     T & select (int i, int j);
     const T & select (int i, int j);
 
   protected:
     T* data;
-    int bi;
-    int bj;
-    unsigned lenght;
+    int basei;
+    int basej;
+    unsigned length;
     unsigned loc(int i, int j) const throw (out_of_range);
 };
 
-Array2D::Array2D()
+Array2D::Array2D(int n = 0, int m = 0, int bi = 0, int bj = 0):
+  data(new T[n*m]), basei(bi), basej(bj), length(n*m){}
+
+Array2D::Array2D(const Array2D &a):
+  data(new T[a.length]), basei(a.basei), basej(a.basej), length(a.length){
+    for (int i = 0, i < length; i++) data[i] = a.data[i];
+  }
+
+Array2D::~Array2D(){
+  delete data[];
+}
+
+
 
