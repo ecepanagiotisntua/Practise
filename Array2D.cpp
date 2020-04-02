2908cc7902fe864d1fd030a45514f323befebf47
ecepanagiotisntua <pkaoukis.2001@yahoo.gr>
2020-04-02 09:02:26 +0000

    2DArrays

--- Array2D.cpp
+++ Array2D.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+class Row;
+
 template<typename T>
 class Array2D{
   public:
@@ -11,33 +13,68 @@ class Array2D{
     ~Array2D();
     Array2D & operator = (const Array2D &a){
       delete data[];
-      data = new T[a.length];
+      data = new T[a.numC*numR];
       basei = a.basei;
       basej = a.basej;
-      length = a.length;
-      for (int i = 0, i < length; i++) data[i] = a.data[i];
+      numC = a.numC;
+      numR = a.numR;
+      for (int i = 0;i < numC*numR; i++) data[i] = a.data[i];
+      return *this;
     }
-    T & select (int i, int j);
-    const T & select (int i, int j);
+    T & select (int i, int j){
+      return data[loc(i, j)];
+
+    }
+    
+    const T & select (int i, int j){
+      return data[loc(i, j)];
+    }
+
+    Row operator [] (int i){
+      return (*this, i);
+    }
 
   protected:
     T* data;
     int basei;
     int basej;
-    unsigned length;
+    unsigned numC;
+    unsigned numR;
     unsigned loc(int i, int j) const throw (out_of_range);
 };
 
+class Row{
+  public:
+    Row(Array2D &a, int i):
+      array2D(a), row(i){};
+    T &operator [](int i) const{
+      return array2D.select(row, i);
+    }
+
+  private:
+    Array2D &array2D;
+    int row;
+
+};
+
 Array2D::Array2D(int n = 0, int m = 0, int bi = 0, int bj = 0):
-  data(new T[n*m]), basei(bi), basej(bj), length(n*m){}
+  data(new T[n*m]), basei(bi), basej(bj), numC(m), numR(i){}
 
 Array2D::Array2D(const Array2D &a):
-  data(new T[a.length]), basei(a.basei), basej(a.basej), length(a.length){
-    for (int i = 0, i < length; i++) data[i] = a.data[i];
+  data(new T[a.length]), basei(a.basei), basej(a.basej), numC(a.numC), numR(a.numR){
+    for (int i = 0, i < numC*numR; i++) data[i] = a.data[i];
   }
 
 Array2D::~Array2D(){
   delete data[];
+}
+
+
+unsigned Array2D::loc(int i, int j) const throw (out_of_range){
+  int di = i-basei;
+  int dj = j-basej;
+  if (di < 0 || dj < 0 || di > numR || dj > numC) throw out_of_range("invalid range");
+  return numR*di + dj;
 }
 
 
